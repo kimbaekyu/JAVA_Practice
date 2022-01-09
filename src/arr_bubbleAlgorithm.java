b9f2ada1c76df6df8442f493a574c2a4b537c27c
kimbaekyu <qordb78@gmail.com>
2022-01-09 18:44:42 +0900

    java arr 실습

---
+++ src/arr_bubbleAlgorithm.java
@@ -0,0 +1,26 @@
+public class arr_bubbleAlgorithm {
+    public static void main(String[] args){
+        int[] arr=new int[10];
+        for(int i=0;i<arr.length;i++){
+            System.out.printf("arr[%d]: %d ",i,arr[i]=(int)(Math.random()*10));
+        }
+        System.out.println();
+        for(int i=0;i<arr.length-1;i++){
+            boolean changed=false;
+            for(int j=0;j<arr.length-1-i;j++){
+                if(arr[j]>arr[j+1]){
+                    int tmp=arr[j];
+                    arr[j]=arr[j+1];
+                    arr[j+1]=tmp;
+                    changed=true;
+                }
+
+            }
+            if(!changed){
+                break;
+            }
+        }
+        for(int k=0;k<arr.length;k++)
+            System.out.print(arr[k]);
+    }
+}
